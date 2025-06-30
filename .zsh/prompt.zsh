9368344cff215dc38c61c1f29b956ffc83ad5b0a
Aliaks <vlasenkoalex14@gmail.com>
2025-06-30 19:03:27 -0400

    Replace .zshrc and add .zsh/ for future plugins

---
+++ .zsh/prompt.zsh
@@ -0,0 +1,46 @@
+git_status=""
+branch=""
+staged_changes=""
+prefix="<"
+suffix=">"
+
+inGitDirectory=false
+
+setPrompt() {
+    PROMPT="╭─%F{green}%n@%m%f %F{blue}%~%f %F{yellow}$git_status%f
+╰─$ "
+    RPROMPT="%F{red}%? ↵%f"
+}
+
+chpwd () {  # Run on cd
+    if [[ -d "$(pwd)/.git" ]]; then
+        inGitDirectory=true
+    else
+        inGitDirectory=false
+    fi
+}
+
+precmd() {
+    if [[ $inGitDirectory == true ]]; then
+        updateGitStatus
+    fi
+    setPrompt
+}
+
+updateGitStatus() {
+    branch=$(git branch --show-current)
+
+    # if [[ $(git diff --cached --name-only | wc -l) > 0 ]]; then
+    #     staged_changes="%F{red}●%F{yellow}"
+    # else
+    #     staged_changes=""
+    # fi
+ 
+    if [[ -n "$(git status --porcelain)" ]]; then
+        staged_changes="%F{red}●%F{yellow}"
+    else
+        staged_changes=""
+    fi
+
+    git_status="$prefix$branch$staged_changes$suffix"
+}
