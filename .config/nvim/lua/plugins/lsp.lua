c9bae972542c3a7bc9d2c7304248c6b3f001e05f
Aliaks <vlasenkoalex14@gmail.com>
2025-07-22 21:42:38 -0400

    Add QML to nvim LSP and TS

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -207,7 +207,6 @@ return {
       --
       -- But for many setups, the LSP (`ts_ls`) will work just fine
       ts_ls = {},
-
       bashls = {},
       html = {},
       cssls = {},
@@ -220,7 +219,6 @@ return {
       jsonls = {},
       markdown_oxide = {},
       pylsp = {},
-
       lua_ls = {
         -- cmd = { ... },
         -- filetypes = { ... },
@@ -281,6 +279,10 @@ return {
           })
         end,
       },
+    })
+
+    require('lspconfig').qmlls.setup({
+      cmd = { 'qmlls6', '-E' },
     })
   end,
 }
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -19,6 +19,7 @@ return { -- Highlight, edit, and navigate code
       'markdown',
       'markdown_inline',
       'query',
+      'qmljs',
       'typescript',
       'vim',
       'vimdoc',
