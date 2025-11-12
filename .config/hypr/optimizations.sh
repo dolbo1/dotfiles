1d09a4a7b6cb6524064b5288b0f1bb99e3f658ec
Aliaks <vlasenkoalex14@gmail.com>
2025-11-12 11:32:12 -0500

    Fix typo and add font size to hypr/optimizations.sh

--- .config/hypr/optimizations.sh
+++ .config/hypr/optimizations.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 HYPRLAPTOP=$(hyprctl getoption decoration:blur:enabled | awk 'NR==1{print $2}')
-if [[ $HYPRLAPTOP == 1]]; then
-    hyprctl notify 1 5000 "rgb(b0b846)" "Optimizations: ON"
+if [[ $HYPRLAPTOP == 1 ]]; then
+    hyprctl notify 1 5000 "rgb(b0b846)" "fontsize:24 Optimizations: ON"
     hyprctl keyword plugin:hyprbars:bar_blur false
     hyprctl keyword decoration:shadow:enabled false
     hyprctl keyword decoration:blur:enabled false
     hyprctl keyword misc:vfr true
     exit 0
 else
-    hyprctl notify 1 5000 "rgb(f2594b)" "Optimizations: OFF"
+    hyprctl notify 1 5000 "rgb(f2594b)" "fontsize:24 Optimizations: OFF"
     hyprctl reload
     exit 0
 fi
